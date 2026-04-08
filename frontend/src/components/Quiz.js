43dc507289e4c6724692b8993b50f4d5dc448e0e
brianman9319-pixel <brianman9319@gmail.com>
2026-04-09 06:25:12 +0900

    Add Quiz component

---
+++ frontend/src/components/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            <h1>Quiz Component</h1>
+            {/* Add quiz questions and logic here */}
+        </div>
+    );
+};
+
+export default Quiz;
